4d43a6e01edd730a5e4c32defb02bcb6e052d94a
korallia <KoralliaFrenette@gmail.com>
2024-05-06 11:19:31 -0400

    test

---
+++ locationApp/src/test/java/com/locationApp/LocationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
